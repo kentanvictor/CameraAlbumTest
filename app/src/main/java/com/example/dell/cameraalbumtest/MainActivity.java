703f4e11e6df7fc0971d52ca76d6eba38237e2d4
kentanvictor <2030457926@qq.com>
2017-06-03 14:23:32 +0800

    third commit

--- app/src/main/java/com/example/dell/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/dell/cameraalbumtest/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.dell.cameraalbumtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int TAKE_PHOTO = 1;
+    private ImageView picture;
+    private Uri imageuri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button takephoto = (Button) findViewById(R.id.take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+        takephoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
