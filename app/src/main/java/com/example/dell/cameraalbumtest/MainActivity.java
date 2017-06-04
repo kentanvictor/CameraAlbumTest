5e2279ba5ce1027eb1c3fa9bd83ee30c2edb047b
kentanvictor <2030457926@qq.com>
2017-06-04 18:06:32 +0800

    fifth commit

--- app/src/main/java/com/example/dell/cameraalbumtest/MainActivity.java
+++ app/src/main/java/com/example/dell/cameraalbumtest/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int TAKE_PHOTO = 1;
     private ImageView picture;
     private Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,48 +32,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //创建File对象，用于存储拍照后的照片
-                File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
+                File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
                 //创建一个File对象，用于存放摄像头拍到的照片，并把图片命名为output_image.jpg
                 //将它存放在手机SD卡的应用关联缓存目录下
                 //getExternalCacheDir()方法可以的到目录
-                try
-                {
-                    if(outputImage.exists()){
+                try {
+                    if (outputImage.exists()) {
                         outputImage.delete();
                     }
                     outputImage.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                if(Build.VERSION.SDK_INT >= 24){
-                    imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.dell.cameraalbumtest.fileprovider",outputImage);
-                }
-                else
-                {
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.dell.cameraalbumtest.fileprovider", outputImage);
+                } else {
                     imageUri = Uri.fromFile(outputImage);
                 }
                 //启动相机程序
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
-                startActivityForResult(intent,TAKE_PHOTO);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
             }
         });
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,Intent data)
-    {
-        switch (requestCode)
-        {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
             case TAKE_PHOTO:
-                if(resultCode == RESULT_OK)
-                {
-                    try{
+                if (resultCode == RESULT_OK) {
+                    try {
                         //将拍摄的照片显示出来
                         Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
                         picture.setImageBitmap(bitmap);
-                    }
-                    catch (FileNotFoundException e)
-                    {
+                    } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
                 }
